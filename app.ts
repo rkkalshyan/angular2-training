85374b353043d13124d2f388100cbc96e10329b8
rishabh <rishabhk@iitrpr.ac.in>
2016-10-02 03:42:49 +0530

    added index and app file

--- app.ts
+++ app.ts
@@ -1,1 +1,29 @@
 // your code goes here
+import { NgModule, Componet } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {PlatformBrowserDynamic, platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+
+
+@Componet({
+   selector: 'hello-world',
+    template: `
+    <div>
+        Hello World
+    </div>
+    `
+})
+
+class HelloWorld {
+
+}
+
+@NgModule({
+    declarations: [ HelloWorld ],
+    imports: [BrowserModule],
+    bootstrap: [HelloWorld]
+})
+
+class HelloWorldAppModule {}
+
+platformBrowserDynamic().bootstrapModule(HelloWorldAppModule);
